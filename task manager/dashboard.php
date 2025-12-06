ae259d0d775a1807d9af1aaff72bebdca83acd0d
nisaazzahwa <nisazzhw@gmail.com>
2025-12-06 10:26:32 +0700

    Update dashboard.php

--- task manager/dashboard.php
+++ task manager/dashboard.php
@@ -71,7 +71,7 @@ $kebiasaan = $konek->query("
 
             <!-- CARD KEBIASAAN -->
             <div class="card">
-                <div class="card-header bg-success text-white">
+               <div class="card-header bg-primary text-white">
                     <h3 class="card-title">Kebiasaan Terbaru</h3>
                 </div>
 
