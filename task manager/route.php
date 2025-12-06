160c8126c463d76da7dfe9a13ca937d73b38fa3f
nisaazzahwa <nisazzhw@gmail.com>
2025-12-06 10:31:15 +0700

    Refactor routing logic in route.php

--- task manager/route.php
+++ task manager/route.php
@@ -2,28 +2,16 @@
 $p = $_GET['p']??'';
 
 switch ($p) {
-    case 'dosen':
-        require_once "dosen.php";
-        break;
-    case 'mahasiswa':
-        require_once "mahasiswa.php";
-        break;
+    case 'dashboard':
+        require_once "Dashboard.php";
+        break; 
+    case 'kebiasaan':
+        require_once "Kebiasaan.php";
+        break;  
     case 'tugas':
-        require_once "tugas.php";
+        require_once "Tugas.php";
         break;
-    case 'delete':
-        require_once "delete.php";
-        break;
-    case 'prodi':
-        require_once "prodi.php";
-        break;
-    case 'jadwal_kuliah':
-        require_once "jadwal_kuliah.php";
-        break;
-    case'prodi':
-        require_once "prodi.phpp";
-        break;
-
+  
     default:
         require_once "Dashboard.php";
         break;
