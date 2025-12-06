09c995040b8d21eb8d291cbea1cf98c7986ff238
Bandar Abdulrab <bandarabdulrab@gmail.com>
2025-12-06 11:05:57 +0700

    Add new routes for chart and delete functionalities

--- task manager/route.php
+++ task manager/route.php
@@ -3,15 +3,20 @@ $p = $_GET['p']??'';
 
 switch ($p) {
     case 'dashboard':
-        require_once "Dashboard.php";
-        break; 
-    case 'kebiasaan':
-        require_once "Kebiasaan.php";
-        break;  
-    case 'tugas':
-        require_once "Tugas.php";
+        require_once "dashboard.php";
         break;
-  
+    case 'kebiasaan':
+        require_once "kebiasaan.php";
+        break;
+    case 'tugas':
+        require_once "tugas.php";
+        break;
+    case 'chart':
+        require_once "chart.php";
+        break;
+    case 'delete':
+        require_once "delete.php";
+        break;
     default:
         require_once "Dashboard.php";
         break;
